582e8be688d4d7d5ed33697f4c3b49d2a5e6a807
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-04-22 20:28:59 +0900

    blank tests

---
+++ src/main/java/demo/IBinaryOperation.java
@@ -0,0 +1,7 @@
+package demo;
+
+import java.util.function.BiFunction;
+
+public interface IBinaryOperation<T, U, R> extends BiFunction<T, U, R> {
+
+}
---
+++ src/main/java/demo/operation/internal/IntegerAddition.java
@@ -0,0 +1,13 @@
+package demo.operation.internal;
+
+import demo.IBinaryOperation;
+
+public class IntegerAddition implements IBinaryOperation<Integer, Integer, Integer> {
+
+	@Override
+	public Integer apply(Integer t, Integer u) {
+		return null;
+	}
+
+
+}
